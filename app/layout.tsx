f346f9994e571fc05060d4239b970c72ce523436
Mintzy <alexgatesent@gmail.com>
2026-02-16 15:52:36 -0700

    update GA4 to correct web property G-4098V4Y0J4

--- app/layout.tsx
+++ app/layout.tsx
@@ -77,8 +77,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         {/* Google Tag Manager */}
         <Script id="gtm" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PXDXPXB9');` }} />
         {/* GA4 direct install as backup */}
-        <Script src="https://www.googletagmanager.com/gtag/js?id=G-4Y7ESH3S0P" strategy="afterInteractive" />
-        <Script id="ga4" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-4Y7ESH3S0P');` }} />
+        <Script src="https://www.googletagmanager.com/gtag/js?id=G-4098V4Y0J4" strategy="afterInteractive" />
+        <Script id="ga4" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-4098V4Y0J4');` }} />
               {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive" dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','1621445598880955');fbq('track','PageView');` }} />
       </head>
