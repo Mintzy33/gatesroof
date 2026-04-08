9fef4c635fbcf868c91125bd032f6284f202cc6f
Mintzy <alexgatesent@gmail.com>
2026-04-08 14:39:35 -0600

    fix: resolve fbq global declaration conflict (TS2687)

--- app/contact/content.tsx
+++ app/contact/content.tsx
@@ -5,7 +5,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { ScrollReveal } from "../components/GSAPAnimations";
 
-declare global { interface Window { fbq: (...args: unknown[]) => void } }
+declare global { interface Window { fbq?: (...args: unknown[]) => void } }
 
 const NAVY = "#06263f";
 const DEEP = "#0D2137";
