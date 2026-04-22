6f5687a06dc3593db9f978031c9a0d36011f228c
Mintzy <alexgatesent@gmail.com>
2026-04-22 14:25:33 -0600

    feat: replace header video with A Wing V4 (GATES_ROOFING_V4_no_text)

--- app/components/HeroVideo.tsx
+++ app/components/HeroVideo.tsx
@@ -2,8 +2,8 @@
 import { useRef, useState } from "react";
 import Image from "next/image";
 
-const VIDEO_URL = "https://res.cloudinary.com/dyr5ihrer/video/upload/q_auto,f_mp4,w_1280,br_1500k/v1771207837/gatesroof.com_Header_on1ccl.mov";
-const VIDEO_POSTER = "https://res.cloudinary.com/dyr5ihrer/video/upload/q_40,f_webp,w_800,so_0/v1771207837/gatesroof.com_Header_on1ccl.mov";
+const VIDEO_URL = "https://res.cloudinary.com/dyr5ihre/video/upload/q_auto,f_mp4,w_1280,br_1500k/v1776889523/gatesroof_header_v4.mp4";
+const VIDEO_POSTER = "https://res.cloudinary.com/dyr5ihrer/video/upload/q_40,f_webp,w_800,so_0/v1776889523/gatesroof_header_v4.mp4";
 
 export default function HeroVideo() {
   const videoRef = useRef<HTMLVideoElement>(null);
